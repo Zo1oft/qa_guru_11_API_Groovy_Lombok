df8b1f4807afaedc5f37581d1cdf919e4a812de5
Max Zhurkin <99205557+Zo1oft@users.noreply.github.com>
2022-05-24 22:30:54 +0500

    Add files via upload

--- src/test/java/tests/models/MorpheusData.java
+++ src/test/java/tests/models/MorpheusData.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-    @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public class MorpheusData {
-        @JsonProperty("morpheusdata")
-        private Morpheus [] morpheus;
-    }
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MorpheusData {
+    @JsonProperty("morpheusdata")
+    private Morpheus[] morpheus;
+}
