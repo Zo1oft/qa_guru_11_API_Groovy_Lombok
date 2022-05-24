b762dfbc99d4ebf42de23521ae1c0f34ca77ce69
Max Zhurkin <99205557+Zo1oft@users.noreply.github.com>
2022-05-24 22:29:30 +0500

    Add files via upload

--- src/test/java/tests/ApiTests.java
+++ src/test/java/tests/ApiTests.java
@@ -1,81 +1,25 @@
 package tests;
 
-import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import lombok.Data;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import tests.models.Morpheus;
-import tests.models.UserData;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.RestAssured.get;
 import static io.restassured.http.ContentType.JSON;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static tests.Specs.MorpheusSpecs.requestSpec;
-import static tests.Specs.MorpheusSpecs.responsemorpheusSpec;
-import static tests.Specs.Specs.request;
-import static tests.Specs.Specs.responseSpec;
 
 @Data
 public class ApiTests {
 
-  @Test
-  @DisplayName("Проверка наличия списка пользователей на странице с помощью Lambok")
-  public void lombokListTest() {
-       UserData data = given()
-                .spec(request)
-                .when()
-                .get("/api/unknown")
-                .then()
-                .spec(responseSpec)
-                    .log().body()
-                    .extract().as(UserData.class);
-        assertEquals(1, data.getUser()[0].getId());
-    }
-
-    @Test
-    @DisplayName("Создание пользователя с именем Morpheus")
-    public void CreateMorpheus () {
-            Morpheus morpheus = new Morpheus();
-            morpheus.setName("morpheus");
-            morpheus.setJob("leader");
-
-           given()
-                    .spec(requestSpec)
-                    .body(morpheus)
-                    .when()
-                    .post("morpheusdata")
-                    .then()
-                    .spec(responsemorpheusSpec)
-                    .body(matchesJsonSchemaInClasspath("morpheusdata.json"))
-                    .extract().as(Morpheus.class);
-    }
-
-    @Test
-    @DisplayName("Проверка наличия данных на странице с помощью Groovy")
-    public void listTestWithGroovy() {
-       given()
-                .spec(request)
-                .when()
-                .get("/api/unknown")
-                .then()
-                .spec(responseSpec)
-                .log().body()
-                .body("data.findAll{it}.year.flatten()",
-                        Matchers.hasItem(2005));
-    }
-
     @BeforeAll
     static void setUp() {
         RestAssured.baseURI = "https://reqres.in/";
-        RestAssured.filters(new AllureRestAssured());
     }
+
     @Test
     @DisplayName("Проверка работоспособности 404 страницы")
     void singleUserNotFound() {
